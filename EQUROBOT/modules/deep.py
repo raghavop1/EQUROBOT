4aeb0f4df2339ed38ad118683a11e7a06e64487a
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-13 12:52:03 +0530

    Update deep.py

--- EQUROBOT/modules/deep.py
+++ EQUROBOT/modules/deep.py
@@ -12,6 +12,8 @@ openai.api_key = config.GPT_API
 
 api_key = config.DEEP_API
 
+API_URL = "https://sugoi-api.vercel.app/search"
+
 # ----------------------------------------
 # ----------------------------------------
 @app.on_message(filters.command(["chatgpt","ai","ask"],  prefixes=["+", ".", "/", "-", "?", "$","#","&"]))
@@ -87,7 +89,7 @@ async def chat(app :app, message):
 # --------------------------------------------------------------------------------
 # --------------------------------------------------------------------------------
 
-@app.on_message(filters.command(["deep" , ],  prefixes=["+", ".", "/", "-", "?", "$", "#", "&"]))
+@app.on_message(filters.command(["deep"],  prefixes=["+", ".", "/", "-", "?", "$", "#", "&"]))
 async def deepchat(app: app, message):
     name = message.from_user.first_name
     try:
@@ -142,3 +144,32 @@ async def deepchat(app: app, message):
         await message.reply_text(f"ᴇʀʀᴏʀ: {e}")
 
 # -----------------------------------------------------------------------------------
+
+@app.on_message(filters.command(["bing"],  prefixes=["+", ".", "/", "-", "?", "$", "#", "&"]))
+async def bing_search(app, message):
+    try:
+        if len(message.command) == 1:
+            await message.reply_text("Please provide a keyword to search.")
+            return
+
+        keyword = " ".join(
+            message.command[1:]
+        )  # Assuming the keyword is passed as arguments
+        params = {"keyword": keyword}
+        response = r.get(API_URL, params=params)
+
+        if response.status_code == 200:
+            results = response.json()
+            if not results:
+                await message.reply_text("No results found.")
+            else:
+                message_text = ""
+                for result in results[:7]:
+                    title = result.get("\x74\x69\x74\x6C\x65", "")
+                    link = result.get("\x6C\x69\x6E\x6B", "")
+                    message_text += f"{title}\n{link}\n\n"
+                await message.reply_text(message_text.strip())
+        else:
+            await message.reply_text("Sorry, something went wrong with the search.")
+    except Exception as e:
+        await message.reply_text(f"An error occurred: {str(e)}")
