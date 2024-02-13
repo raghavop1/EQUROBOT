2239f2fcc882c6f64f659e121aa0e879e644e1b3
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-13 12:13:25 +0530

    Delete EQUROBOT/modules/bot directory

--- EQUROBOT/modules/bot/alive.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import Client, filters
-from EQUROBOT import app
-# ---------------------------
-@app.on_message(filters.command("alive", prefixes="."))
-def alive(_, message):
-    message.reply_text("I'm alive and kicking!")
