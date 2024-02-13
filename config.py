6dd8bf404a321c1f3d6b637dbe55741c15129115
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-13 11:55:24 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+
+# --------------M----------------------------------
+
+import os
+from os import getenv
+# ---------------R---------------------------------
+API_ID = int(os.environ.get("API_ID"))
+# ------------------------------------------------
+API_HASH = os.environ.get("API_HASH")
+# ----------------D--------------------------------
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+# -----------------A-------------------------------
+BOT_USERNAME = os.environ.get("BOT_USERNAME")
+# ------------------X------------------------------
+OWNER_ID = int(os.environ.get("OWNER_ID"))
+# ------------------X------------------------------
+DEEP_API = os.environ.get("DEEP_API")
+# ------------------------------------------------
+LOGGER_ID = int(os.environ.get("LOGGER_ID"))
+# ------------------------------------------------
+GPT_API = os.environ.get("GPT_API")
+# ------------------------------------------------
+DAXX_API = os.environ.get("DAXX_API")
