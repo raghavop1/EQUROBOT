b2f4fadb2642addae19949b7af49940150849c7e
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-14 16:07:52 +0530

    Update start.py

--- EQUROBOT/modules/start.py
+++ EQUROBOT/modules/start.py
@@ -1,1 +1,42 @@
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto
+from config import BOT_USERNAME, OWNER_ID
+from pyrogram.types import InlineKeyboardButton as ib
+import asyncio
+from EQUROBOT import app
 
+
+START_TEXT = """
+ʜɪ [user.mention],
+
+ɪ ᴀᴍ {app.username}, 
+ʏᴏᴜʀ ᴀɪ ᴄᴏᴍᴘᴀɴɪᴏɴ. 
+ʟᴇᴛ'ꜱ ᴄʜᴀᴛ ᴀɴᴅ ᴇxᴘʟᴏʀᴇ 
+ᴛʜᴇ ᴅᴇᴘᴛʜꜱ ᴏꜰ ᴄᴏɴᴠᴇʀꜱᴀᴛɪᴏɴ ᴛᴏɢᴇᴛʜᴇʀ! 
+ꜰᴇᴇʟ ꜰʀᴇᴇ ᴛᴏ ᴀꜱᴋ ᴍᴇ ᴀɴʏᴛʜɪɴɢ ᴏʀ ꜱʜᴀʀᴇ ʏᴏᴜʀ ᴛʜᴏᴜɢʜᴛꜱ. 
+ɪ'ᴍ ʜᴇʀᴇ ᴛᴏ ʟɪꜱᴛᴇɴ ᴀɴᴅ ᴇɴɢᴀɢᴇ ɪɴ ᴍᴇᴀɴɪɴɢꜰᴜʟ ᴅɪꜱᴄᴜꜱꜱɪᴏɴꜱ ᴡɪᴛʜ ʏᴏᴜ."
+"""
+# ------------------------------------------------------------------------------- #
+
+
+
+
+@app.on_message(filters.command("start") & filters.private)
+async def start(client, message):
+    buttons = [
+        [
+            InlineKeyboardButton("⦿ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ⦿", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+        ],
+        [
+            InlineKeyboardButton("⦿ɢʀᴏᴜᴘ⦿", url=f"https://t.me/ALLTYPECC"),
+            InlineKeyboardButton("⦿ᴏᴡɴᴇʀ⦿", user_id=OWNER_ID)
+        ]
+    ]
+
+    reply_markup = InlineKeyboardMarkup(buttons)
+
+    await message.reply_video(
+        video="https://graph.org/file/5690109178f081adf464d.mp4",
+        caption=START_TEXT,
+        reply_markup=reply_markup
+    )
