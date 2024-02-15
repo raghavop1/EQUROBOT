46a3bfbb2098205ef1188a5c705e453a978dcae8
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-15 16:41:38 +0530

    Update image.py

--- EQUROBOT/modules/image.py
+++ EQUROBOT/modules/image.py
@@ -9,35 +9,40 @@ from uuid import uuid4
 from pyrogram.types import InlineKeyboardButton,InlineKeyboardMarkup
 
 
-@app.on_message(filters.reply & filters.command("upscale"))
-async def upscale_image(app, message):
-    try:
-        if not message.reply_to_message or not message.reply_to_message.photo:
-            await message.reply_text("**ᴘʟᴇᴀsᴇ ʀᴇᴘʟʏ ᴛᴏ ᴀɴ ɪᴍᴀɢᴇ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ɪᴛ.**")
-            return
+import aiofiles, aiohttp, requests
 
-        image = message.reply_to_message.photo.file_id
-        file_path = await app.download_media(image)
 
-        with open(file_path, "rb") as image_file:
-            f = image_file.read()
+async def image_loader(image: str, link: str):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(link) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(image, mode="wb")
+                await f.write(await resp.read())
+                await f.close()
+                return image
+            return image
+            
 
-        b = base64.b64encode(f).decode("utf-8")
-
-        async with httpx.AsyncClient() as http_client:
-            response = await http_client.post(
-                "https://api.qewertyy.me/upscale", data={"image_data": b}, timeout=None
-            )
-
-        with open("upscaled.png", "wb") as output_file:
-            output_file.write(response.content)
-
-        await client.send_document(
-            message.chat.id,
-            document="upscaled.png",
-            caption="**ʜᴇʀᴇ ɪs ᴛʜᴇ ᴜᴘsᴄᴀʟᴇᴅ ɪᴍᴀɢᴇ!**",
-        )
-
-    except Exception as e:
-        print(f"**ғᴀɪʟᴇᴅ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ᴛʜᴇ ɪᴍᴀɢᴇ**: {e}")
-        await message.reply_text("**ғᴀɪʟᴇᴅ ᴛᴏ ᴜᴘsᴄᴀʟᴇ ᴛʜᴇ ɪᴍᴀɢᴇ. ᴘʟᴇᴀsᴇ ᴛʀʏ ᴀɢᴀɪɴ ʟᴀᴛᴇʀ**.")
+@bot.on_message(cdx(["upscale"]))
+async def upscale_image(client, message):
+    chat_id = message.chat.id
+    replied = message.reply_to_message
+    if not vars.DEEP_API_KEY:
+        return await message.reply_text("I can't upscale !")
+    if not replied:
+        return await message.reply_text("Please Reply To An Image ...")
+    if not replied.photo:
+        return await message.reply_text("Please Reply To An Image ...")
+    aux = await message.reply_text("Please Wait ...")
+    image = await replied.download()
+    data = requests.post(
+        "https://api.deepai.org/api/torch-srgan",
+        files={
+            'image': open(image, 'rb'),
+        },
+        headers={'api-key': config.DEEP_API_KEY}
+    ).json()
+    image_link = data["output_url"]
+    downloaded_image = await image_loader(image, image_link)
+    await aux.delete()
+    return await message.reply_document(downloaded_image)
