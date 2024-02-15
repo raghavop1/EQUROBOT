db5e56bcb9fa47723f883418826fdde7f5d08be5
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-16 00:28:54 +0530

    Update download.py

--- EQUROBOT/modules/download.py
+++ EQUROBOT/modules/download.py
@@ -1,22 +1,13 @@
 import os
-import future
 import asyncio
 import requests
 import wget
-import time
 import yt_dlp
-from urllib.parse import urlparse
 from youtube_search import YoutubeSearch
 from yt_dlp import YoutubeDL
-
-from EQUROBOT import app
 from pyrogram import filters
-from pyrogram import Client, filters
-from pyrogram.types import Message
-from youtubesearchpython import VideosSearch
-from youtubesearchpython import SearchVideos
-import future
-
+from pyrogram.types import *
+from EQUROBOT import app
 
 #-------------------
 
