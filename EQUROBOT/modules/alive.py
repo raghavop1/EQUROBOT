41a299eba27ff0d4e5266a160d7955be6eb858b0
ᴹᴿᴅᴀxx ࿐™ <147141400+DAXXTEAM@users.noreply.github.com>
2024-02-15 17:34:26 +0530

    Update alive.py

--- EQUROBOT/modules/alive.py
+++ EQUROBOT/modules/alive.py
@@ -4,4 +4,4 @@ from EQUROBOT import app
 # ---------------------------
 @app.on_message(filters.command("alive", prefixes="."))
 def alive(_, message):
-    message.reply_text("I'm alive and kicking!")
+    message.reply_text("🥀 I Aᴍ Aʟɪᴠᴇ Mʏ Dᴇᴀʀ Gᴇɴɪᴜs Mᴀsᴛᴇʀ ✨ ...!")
