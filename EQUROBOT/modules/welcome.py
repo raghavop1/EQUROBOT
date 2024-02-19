55f2dce75c52bdc936639c94767e2487d8d84d4b
STD DEEPANSHU <108056384+STD-DEEPANSHU@users.noreply.github.com>
2024-02-19 20:11:58 +0530

    Update welcome.py

--- EQUROBOT/modules/welcome.py
+++ EQUROBOT/modules/welcome.py
@@ -78,13 +78,13 @@ def circle(pfp, size=(500, 500), brightness_factor=10):
     return pfp
 
 def welcomepic(pic, user, chatname, id, uname, brightness_factor=1.3):
-    background = Image.open("DAXXMUSIC/assets/wel2.png")
+    background = Image.open("EQUROBOT/assets/wel2.png")
     pfp = Image.open(pic).convert("RGBA")
     pfp = circle(pfp, brightness_factor=brightness_factor) 
     pfp = pfp.resize((575, 575))
     draw = ImageDraw.Draw(background)
-    font = ImageFont.truetype('DAXXMUSIC/assets/font.ttf', size=70)
-    welcome_font = ImageFont.truetype('DAXXMUSIC/assets/font.ttf', size=61)
+    font = ImageFont.truetype('EQUROBOT/assets/font.ttf', size=70)
+    welcome_font = ImageFont.truetype('EQUROBOT/assets/font.ttf', size=61)
     #draw.text((630, 540), f'ID: {id}', fill=(255, 255, 255), font=font)
     #
  #   draw.text((630, 300), f'NAME: {user}', fill=(255, 255, 255), font=font)
@@ -149,7 +149,7 @@ async def greet_new_member(_, member: ChatMemberUpdated):
                 user.photo.big_file_id, file_name=f"pp{user.id}.png"
             )
         except AttributeError:
-            pic = "DAXXMUSIC/assets/upic.png"
+            pic = "EQUROBOT/assets/upic.png"
         if (temp.MELCOW).get(f"welcome-{member.chat.id}") is not None:
             try:
                 await temp.MELCOW[f"welcome-{member.chat.id}"].delete()
